6b00dda43028dd59eebb5ac3ace38b64e9b956f0
Anonymousbeliever <[jkcharo18@gmail.com protected]>
2025-09-19 07:57:18 +0300

    added gesture detector

--- lib/screens/add_expense/views/add_expense.dart
+++ lib/screens/add_expense/views/add_expense.dart
@@ -5,34 +5,39 @@ class AddExpense extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-       backgroundColor: Theme.of(context).colorScheme.background,
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.background,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              "Add Expenses",
-              style: TextStyle(
-                fontSize: 22,
-                fontWeight: FontWeight.bold,
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+         backgroundColor: Theme.of(context).colorScheme.background,
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.background,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "Add Expenses",
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            TextFormField(),
-            SizedBox(height: 16),
-            TextFormField(),
-            SizedBox(height: 16),
-            TextFormField(),
-            SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text("Add Expense"),
-            ),
-          ],
+              TextFormField(),
+              SizedBox(height: 16),
+              TextFormField(),
+              SizedBox(height: 16),
+              TextFormField(),
+              SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text("Add Expense"),
+              ),
+            ],
+          ),
         ),
       ),
     );
