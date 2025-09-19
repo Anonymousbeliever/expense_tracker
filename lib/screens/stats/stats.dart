eabbc73e499f2a6ad821c0490765c1d67de20c2d
Anonymousbeliever <[jkcharo18@gmail.com protected]>
2025-09-19 07:32:41 +0300

    created add expense file

--- lib/screens/stats/stats.dart
+++ lib/screens/stats/stats.dart
@@ -1,5 +1,4 @@
 import 'package:expense_tracker/screens/stats/chart.dart';
-import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
 class StatScreen extends StatelessWidget {
@@ -18,7 +17,6 @@ class StatScreen extends StatelessWidget {
               style: TextStyle(
                 fontSize: 24,
                 fontWeight: FontWeight.bold,
-              
               ),
             ),
           const  SizedBox(height: 20),
