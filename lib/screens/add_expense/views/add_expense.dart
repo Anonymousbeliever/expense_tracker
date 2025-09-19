3e9c57a78014a1282d0a34244f60db2bcd737058
Anonymousbeliever <[jkcharo18@gmail.com protected]>
2025-09-19 08:39:23 +0300

    aaded more content

--- lib/screens/add_expense/views/add_expense.dart
+++ lib/screens/add_expense/views/add_expense.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AddExpense extends StatelessWidget {
@@ -10,7 +11,7 @@ class AddExpense extends StatelessWidget {
         FocusScope.of(context).unfocus();
       },
       child: Scaffold(
-         backgroundColor: Theme.of(context).colorScheme.background,
+        backgroundColor: Theme.of(context).colorScheme.background,
         appBar: AppBar(
           backgroundColor: Theme.of(context).colorScheme.background,
         ),
@@ -21,21 +22,70 @@ class AddExpense extends StatelessWidget {
             children: [
               Text(
                 "Add Expenses",
-                style: TextStyle(
-                  fontSize: 22,
-                  fontWeight: FontWeight.bold,
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 16),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.grey.shade300,
+                    prefixIcon: Icon(
+                      CupertinoIcons.money_dollar,
+                      color: Colors.grey.shade500,
+                    ),
+                    hintText: "Amount",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
                 ),
               ),
-              TextFormField(),
               SizedBox(height: 16),
-              TextFormField(),
-              SizedBox(height: 16),
-              TextFormField(),
-              SizedBox(height: 16),
-              ElevatedButton(
-                onPressed: () {},
-                child: Text("Add Expense"),
+              TextFormField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.grey.shade300,
+                  prefixIcon: Icon(
+                    CupertinoIcons.list_bullet,
+                    color: Colors.grey.shade500,
+                  ),
+                  hintText: "Category",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
               ),
+              SizedBox(height: 16),
+              TextFormField(
+                readOnly: true,
+                onTap: () => showDatePicker(
+                  context: context,
+                  initialDate: DateTime.now(),
+                  firstDate: DateTime(2000),
+                  lastDate: DateTime(2100),
+                ).then((selectedDate) {
+                  // Handle the selected date here
+                }),
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.grey.shade300,
+                  prefixIcon: Icon(
+                    CupertinoIcons.calendar,
+                    color: Colors.grey.shade500,
+                  ),
+                  hintText: "Date",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+              SizedBox(height: 16),
+              ElevatedButton(onPressed: () {}, child: Text("Add Expense")),
             ],
           ),
         ),
